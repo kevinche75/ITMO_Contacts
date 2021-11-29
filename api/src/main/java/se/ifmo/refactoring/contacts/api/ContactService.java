aedc7cee85dbe79a054bd7c830e68cf39d6f6d5a
Dmitrii Beliakov <kevinche75@gmail.com>
2021-11-29 22:05:13 +0300

    server auth

--- api/src/main/java/se/ifmo/refactoring/contacts/api/ContactService.java
+++ api/src/main/java/se/ifmo/refactoring/contacts/api/ContactService.java
@@ -1,16 +1,9 @@
 package se.ifmo.refactoring.contacts.api;
 
-import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
-
+import javax.ws.rs.*;
 import java.util.List;
-import javax.ws.rs.BeanParam;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
 @Path("/api/contacts")
 public interface ContactService {
--- server/src/main/java/se/ifmo/refactoring/contacts/server/JerseyConfig.java
+++ server/src/main/java/se/ifmo/refactoring/contacts/server/JerseyConfig.java
@@ -3,6 +3,7 @@ package se.ifmo.refactoring.contacts.server;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import se.ifmo.refactoring.contacts.api.AuthService;
 import se.ifmo.refactoring.contacts.api.ContactService;
 
 @Configuration
@@ -10,6 +11,9 @@ public class JerseyConfig {
 
   @Bean
   public ResourceConfig jaxrsResourceConfig() {
-    return new ResourceConfig().register(ContactService.class);
+    return new ResourceConfig()
+            .register(ContactService.class)
+            .register(AuthService.class)
+            .register(SecurityFilter.class);
   }
 }
